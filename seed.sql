4ba644767d368bd68eb1466df264b881488bcc39
Tajah Johnson <tajah93@hotmail.com>
2020-09-30 20:38:56 -0500

    Updated MySQL

--- seed.sql
+++ seed.sql
@@ -1,15 +1,28 @@
-/* Seeds for SQL table. We haven't discussed this type of file yet */
 USE employee_tracker_db;
 
 INSERT INTO department (name)
 VALUES ("IT"), ("HR");
 
-INSERT INTO role (title, salary)
-VALUES ("Manager", 130000.00), ("Tech Consultant", 70000.00), ("Intern", 25000.00), ("Hiring Manager", 50000)
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 130000.00, 1), ("Tech Consultant", 70000.00, 1), ("Intern", 25000.00, 1), ("Hiring Manager", 50000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id )
-VALUES ("Tajah", "Johnson", 1, 1 ), ("Brenda", "Diaz", 2), ("Lucy", "Castro", 2), ("Claudia", "Albelo", 3), ("Coco Johnson", 4, 1)
+VALUES ("Tajah", "Johnson", 1, 1 ), ("Brenda", "Diaz", 2, null), ("Lucy", "Castro", 2, null), ("Claudia", "Albelo", 3, null), ("Coco", "Johnson", 4, 1);
 
 
+SELECT * FROM role;
+
+SELECT * FROM department;
+
+SELECT department.name, employee.first_name, employee.last_name, role.title, role.salary
+FROM employee
+INNER JOIN role ON role.id = employee.role_id
+INNER JOIN department ON department.id = role.department_id;
+
+UPDATE employee_tracker_db.role 
+SET salary = "70000.00"
+WHERE id = 4;
+
+SELECT * FROM role;
 
 
--- tracker.sql
+++ tracker.sql
@@ -4,13 +4,13 @@ CREATE database employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NULL,
   salary DECIMAL(10,2) NULL,
   department_id INT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NULL,
